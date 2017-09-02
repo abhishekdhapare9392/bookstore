2c052662fa2fbe14d436c08859b1032fddb07715
Abhishek Dhapare <abhishek.dhapare@gmail.com>
2017-09-03 00:03:38 +0530

    Updated Remaining Part

--- client/controllers/books.js
+++ client/controllers/books.js
@@ -3,29 +3,36 @@ var myApp = angular.module('myApp');
 myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
     console.log('BooksController Loded...');
     $scope.getBooks = function() {
-        $http({
-            method: 'GET',
-            url: 'api/books'
-        }).then(function(response) {
+        $http.get('/api/books').then(function(response) {
             $scope.books = response.data;
-            console.log($scope.books);
-            console.log("Letest Books Show Successfully");
-        }, function(error) {
-            console.log("Unable to fetch please check...");
         });
-    };
+    }
 
     $scope.getBook = function() {
         var id = $routeParams.id;
-        $http({
-            method: 'GET',
-            url: 'api/books/' + id
-        }).then(function(response) {
+        $http.get('/api/books/' + id).then(function(response) {
             $scope.book = response.data;
-            console.log($scope.books);
-            console.log("Letest Books Show Successfully");
-        }, function(error) {
-            console.log("Unable to fetch please check...");
+            console.log($scope.book.genre);
         });
     };
+
+    $scope.addBook = function() {
+        $http.post('/api/books/', $scope.book).then(function(response) {
+            window.location.href = '#!/books';
+        });
+    };
+
+    $scope.updateBook = function() {
+        var id = $routeParams.id;
+        $http.put('/api/books/' + id, $scope.book).then(function(response) {
+            window.location.href = '#!/books';
+        });
+    };
+
+    $scope.removeBook = function(id) {
+        $http.delete('/api/books/' + id).then(function(response) {
+            window.location.href = '#!/books';
+        });
+    };
+
 }]);
